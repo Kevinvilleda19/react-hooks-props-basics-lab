968e551960691ee8a2620b6e4f530412482a7286
Kevin <kvilleda10566@gmail.com>
2024-09-04 10:06:35 -0400

    done

---
+++ src/components/Links.js
@@ -0,0 +1,17 @@
+import React from "react";
+import About from "./About";
+function Links(props) {
+    return (
+        <div>
+        <h3>Links</h3>
+        <a href={props.github} target="_blank" rel="noopener noreferrer">
+            {props.github}
+            </a>
+        <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
+            {props.linkedin}
+            </a>
+        </div>
+    );
+}
+
+export default Links;
